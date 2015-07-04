ad491e31326c493ea78e68000991a13cf41a3a41
anjanadesire <anjanadesire@gmail.com>
2015-07-05 01:46:03 +0530

    Update sample.js

--- scripts/services/sample.js
+++ scripts/services/sample.js
@@ -1,3 +1,16 @@
+var json = [
+	
+    {username: 'anju',	password:'happy', projects:'project1,project2'},
+	{username: 'swarna', password:'happy1',projects:'project3,project2'},
+    {username: 'pallavi', password:'happy2',projects:'project1,project4'},
+    {username: 'chintu', password:'happy3',projects:'project5,project2'}
+    
+    
+    ]
+	
+
+
+
 var LoginDetails = React.createClass({
   getInitialState: function() {
     return {
@@ -5,39 +18,89 @@ var LoginDetails = React.createClass({
     };
   },
   
-  handleChange: function(evt) {
+  setUserName: function(evt) {
     this.setState({
-      value: evt.target.value
+      userName: evt.target.value
+     
+    });
+  },
+  setPassword: function(evt) {
+    this.setState({
+      password: evt.target.value
+     
     });
   },
   
   reset: function() {
     this.setState({
-      value: ""
+      userName: "",
+      password:""
+      
     });
   },
   
-  alertValue: function() {
-    alert(this.state.value);
+  
+  validate: function(){
+  var valid = false; 
+  
+
+   for(var i=0;i<json.length;i++)
+   {
+   
+ if((this.state.userName==json[i].username) &&  (this.state.password == json[i].password))
+  {
+  alert(" username matches ");
+  valid= true;
+  
+  }
+  
+  }
+  if(!valid)
+  {
+  alert("Invalid Username or Password");
+    }
   },
+  
   
   render: function() {
     return (
+	
 	<div>
+	
       <div>
 	  <span>EmployeeId:
-        <input type="text" value={this.state.value} onChange={this.handleChange} ></input></span>
-        </div>
+        <input type="text" value={this.state.userName} onChange={this.setUserName} ></input></span>
+        </div><br/>
 		<div>
 		<span>Password:
-		 <input type="text" value={this.state.value} onChange={this.handleChange} ></input></span>
+		 <input type="text" value={this.state.password} onChange={this.setPassword} ></input></span>
 		  </div>
 		  <div>
 		  <button onClick={this.reset}>Reset</button>
-        <button onClick={this.alertValue}>Login</button>
+        <button onClick={this.validate}>Login</button>
+		
+		
+		
+           
+		
+
     </div></div>
+	
     );
   }
+  
+  
+  
+   
+});
+var projects = React.createClass({
+    render: function() {
+        return (
+            <div >
+			<p>Welcome {this.state.userName}</p>
+            </div>
+        );
+    }
 });
 
 
